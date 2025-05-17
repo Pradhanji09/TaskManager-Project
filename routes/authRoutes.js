6103bd1583831ccfbf8a1ce4210ecebac088843a
Pradhanji09 <sou030rav@gmail.com>
2025-05-17 13:23:03 +0530

    middleware added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,19 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controller/authController");
+const {
+  registerUser,
+  loginUser,
+  logoutUser,
+} = require("../controller/authController");
+
+const { isAuth } = require("../middlewares/isAuth");
 
 const router = express.Router();
 
+// Public Routes (no authentication needed)
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+// Protected Route (user must be authenticated to log out)
+router.get("/logout", isAuth, logoutUser);
 
 module.exports = router;
